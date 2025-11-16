63db71fe87f8ebbe71709a300b092d66718d3b95
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2025-11-16 14:29:53 +0600

    Introduction to C++

---
+++ Semister 02 C++ For DSA/Week 01 C++ 101/Class/Print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int X = 10;
+    char C = 'A';
+    double D = 35.56;
+    std ::cout << X << " " << C << std::endl
+               << D << " " << std::endl;
+    std::cout << "My Favorite Number is" << " " << X << std::endl;
+    return 0;
+}
