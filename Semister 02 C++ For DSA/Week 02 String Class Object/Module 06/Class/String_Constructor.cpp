5e4707c5ba12289417bc774c2ec60464fc0568fb
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2025-12-01 23:28:33 +0600

    Module 06

--- Semister 02 C++ For DSA/Week 02 String Class Object/Module 06/Class/String_Constructor.cpp
+++ Semister 02 C++ For DSA/Week 02 String Class Object/Module 06/Class/String_Constructor.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-
+    // string s = "Hello";
+    // string s("Hello");
+    // string s("Hello World", 4);
+    // string s = "Hello world";
+    // string t(s, 2);
+    string s(10, 'B');
+    cout << s << endl;
     return 0;
 }
