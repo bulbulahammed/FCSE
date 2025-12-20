2b12a99eaf8c033c7b9ce006cd43ceab9558df37
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2025-12-20 23:58:30 +0600

    Module 6 String constructor practice

---
+++ Semister 02 C++ For DSA/Week 02 String Class Object/Module 06 More About Class String/Class Practice/String_Constructior.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello World";
+    // 1
+    // string s("Hello world");
+    // 2 Resize using constructor
+    // string s("Hello world", 4);
+    // 3
+    // string s = "Hello world";
+    // string t(s, 4);
+    // 4
+    string s(5, 'A');
+    cout << s << endl;
+    return 0;
+}
